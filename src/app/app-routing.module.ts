2593bc8925c7d6f8b620a289e358f4b74e01d4ad
Daniel Montenegro <dannielmdr7@gmail.com>
2022-07-14 16:44:36 -0500

    feature: home listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProductoComponent } from './views/producto/producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'producto',
+    component: ProductoComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'**',
+    redirectTo:'home'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ProductoComponent } from './views/producto/producto.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    ProductoComponent,
+    HomeComponent,
+    ProductosComponent,
+    CursosComponent,
+    BlogComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
